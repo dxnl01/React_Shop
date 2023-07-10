23e26b37fb9a54ba819cf27b351263c579d2e6d4
GeorgeKonrad29 <136720356+GeorgeKonrad29@users.noreply.github.com>
2023-07-09 23:47:47 -0500

    comentarios añadidos
    solo a los controladores

--- server/controllers/productsControllers.js
+++ server/controllers/productsControllers.js
@@ -8,33 +8,33 @@ import { sendMail } from "../email/mail.js";
 // Controlador para obtener todos los productos
 const getAllProducts = async (req, res) => {
   try {
-    const products = await ProductModel.findAll();
-    res.json(products);
+    const products = await ProductModel.findAll(); // Consulta todos los productos en la base de datos
+    res.json(products); // Envía la respuesta como JSON con la lista de productos
   } catch (error) {
-    res.json({ message: error.message });
+    res.json({ message: error.message }); // Si ocurre un error, envía una respuesta JSON con el mensaje de error
   }
 };
 // Controlador para obtener un producto específico
 const getProduct = async (req, res) => {
   try {
     const product = await ProductModel.findAll({
-      where: { id: req.params.id },
+      where: { id: req.params.id }, // Consulta el producto en la base de datos utilizando el ID proporcionado en los parámetros de la solicitud
     });
-    res.json(product[0]);
+    res.json(product[0]); // Envía la respuesta como JSON con el primer producto encontrado en formato de objeto
   } catch (error) {
-    res.json({ message: error.message });
+    res.json({ message: error.message }); // Si ocurre un error, envía una respuesta JSON con el mensaje de error
   }
 };
 
 // Controlador para crear un nuevo producto
 const createProduct = async (req, res) => {
   try {
-    await ProductModel.create(req.body);
+    await ProductModel.create(req.body); // Crea un nuevo producto en la base de datos utilizando los datos proporcionados en el cuerpo de la solicitud
     res.json({
-      message: "Creación del registro exitosa",
+      message: "Creación del registro exitosa", // Envía una respuesta JSON con un mensaje de éxito
     });
   } catch (error) {
-    res.json({ message: error.message });
+    res.json({ message: error.message }); // Si ocurre un error, envía una respuesta JSON con el mensaje de error
   }
 };
 
@@ -42,13 +42,13 @@ const createProduct = async (req, res) => {
 const updateProducts = async (req, res) => {
   try {
     await ProductModel.update(req.body, {
-      where: { id: req.params.id },
+      where: { id: req.params.id }, // Actualiza el registro del producto en la base de datos con los datos proporcionados en el cuerpo de la solicitud, filtrando por el ID proporcionado en los parámetros de la solicitud
     });
     res.json({
-      message: "El registro se ha actualizado con éxito",
+      message: "El registro se ha actualizado con éxito", // Envía una respuesta JSON con un mensaje de éxito
     });
   } catch (error) {
-    res.json({ message: error.message });
+    res.json({ message: error.message }); // Si ocurre un error, envía una respuesta JSON con el mensaje de error
   }
 };
 
@@ -56,65 +56,73 @@ const updateProducts = async (req, res) => {
 const deleteProduct = async (req, res) => {
   try {
     await ProductModel.destroy(req.body, {
-      where: { id: req.params.id },
+      where: { id: req.params.id }, // Elimina el registro del producto en la base de datos con el ID proporcionado en los parámetros de la solicitud
     });
     res.json({
-      message: "El registro se ha borrado con éxito",
+      message: "El registro se ha borrado con éxito", // Envía una respuesta JSON con un mensaje de éxito
     });
   } catch (error) {
-    res.json({ message: error.message });
+    res.json({ message: error.message }); // Si ocurre un error, envía una respuesta JSON con el mensaje de error
   }
 };
+
+
+
+
+
 
 // Controlador para reservar o desreservar un producto
 const bookProduct = async (req, res) => {
   try {
-    console.log(productsStock);
-    if (req.query.f === "unboox") {
-      productsStock[req.params.id]++;
-      return res.json("Unbooked");
-    } else if (req.query.f === "book") {
-      if (productsStock[req.params.id] == 0)
-        return res.json("Stockout");////en caso de que el producto sea igual a 0 se notifica que se acabo
-      productsStock[req.params.id]--;
-      return res.json("Booked");
+    console.log(productsStock); // Imprime el valor de la variable 'productsStock' en la consola
+    if (req.query.f === "unboox") { // Verifica si el valor del parámetro de consulta 'f' es igual a "unboox"
+      productsStock[req.params.id]++; // Incrementa en 1 el valor correspondiente a la clave 'req.params.id' en el objeto 'productsStock'
+      return res.json("Unbooked"); // Envía una respuesta JSON con el mensaje "Unbooked"
+    } else if (req.query.f === "book") { // Verifica si el valor del parámetro de consulta 'f' es igual a "book"
+      if (productsStock[req.params.id] == 0) // Verifica si el valor correspondiente a la clave 'req.params.id' en el objeto 'productsStock' es igual a 0
+        return res.json("Stockout"); // Si es igual a 0, envía una respuesta JSON con el mensaje "Stockout" indicando que no hay stock
+      productsStock[req.params.id]--; // Decrementa en 1 el valor correspondiente a la clave 'req.params.id' en el objeto 'productsStock'
+      return res.json("Booked"); // Envía una respuesta JSON con el mensaje "Booked"
     }
-    res.status(400).json("Bad request");
+    res.status(400).json("Bad request"); // Si no se cumple ninguna de las condiciones anteriores, envía una respuesta JSON con el mensaje "Bad request" y un código de estado 400 (solicitud incorrecta)
   } catch (error) {
-    res.json({ message: error.message });
+    res.json({ message: error.message }); // Si ocurre un error, envía una respuesta JSON con el mensaje de error
   }
 };
+
 
 
 // Función para actualizar el contenido en la base de datos
 const updateContent = async (product, quantity) => {
   const stock = await ProductModel.findAll({
-    attributes: ["id", "stock"],
-    where: { id: product },
+    attributes: ["id", "stock"], // Se especifican los atributos a incluir en la consulta: "id" y "stock"
+    where: { id: product }, // Se filtra por el ID del producto proporcionado en el parámetro 'product'
   });
-  console.log(quantity);
+  console.log(quantity); // Imprime el valor de la variable 'quantity' en la consola
   await ProductModel.update(
-    { stock: stock[0].dataValues.stock - quantity[product] },
+    { stock: stock[0].dataValues.stock - quantity[product] }, // Actualiza el stock del producto restando la cantidad proporcionada en 'quantity[product]'
     {
-      where: { id: product },
+      where: { id: product }, // Se especifica el filtro para actualizar el producto con el ID proporcionado en 'product'
     }
   );
   if (productMinStock[product].stockMin >= stock[0].dataValues.stock - quantity[product]) {
-    sendMail({ id: product });
+    sendMail({ id: product }); // Si el stock actualizado es menor o igual al stock mínimo definido en 'productMinStock[product].stockMin', se envía un correo electrónico notificando sobre el bajo stock
   }
 };
+
 
 
 // Controlador para comprar productos
 const buyProducts = async (req, res) => {
   try {
-    console.log(typeof req.body);
-    Object.keys(req.body).forEach((product) => updateContent(product, req.body));
-    res.json("Successful purchase");
+    console.log(typeof req.body); // Imprime el tipo de datos de 'req.body' en la consola
+    Object.keys(req.body).forEach((product) => updateContent(product, req.body)); // Itera sobre las claves de 'req.body' y llama a la función 'updateContent' pasando cada clave como 'product' y 'req.body' como argumento
+    res.json("Successful purchase"); // Envía una respuesta JSON con el mensaje "Successful purchase"
   } catch (error) {
-    res.json(error.message);
+    res.json(error.message); // Si ocurre un error, envía una respuesta JSON con el mensaje de error
   }
 };
+
 
 // Exportamos todos los controladores
 export {  
